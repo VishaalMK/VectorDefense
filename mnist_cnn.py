333e9120d42ce460f5a222a6a3ea8400e1fe5a63
VishaalMK <mkvishaal@gmail.com>
2018-04-17 20:36:10 -0500

    minor

---
+++ mnist_cnn.py
@@ -0,0 +1,197 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+import keras
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras import backend
+import tensorflow as tf
+from tensorflow.python.platform import flags
+import scipy.misc
+
+import matplotlib
+matplotlib.use('Agg')
+
+from cleverhans.utils_mnist import data_mnist
+from cleverhans.utils_tf import model_train, model_eval
+from cleverhans.attacks import CarliniWagnerL2
+from cleverhans.utils import AccuracyReport
+from cleverhans.utils_keras import KerasModelWrapper, conv_2d
+
+FLAGS = flags.FLAGS
+
+model_path = "/home/vishaal/workspace/obfuscated-gradients/inputtransformations/models/mnist"
+
+def cnn_model(logits=False, input_ph=None, img_rows=28, img_cols=28,
+              channels=1, nb_filters=64, nb_classes=10):
+    """
+    Defines a CNN model using Keras sequential model
+    :param logits: If set to False, returns a Keras model, otherwise will also
+                    return logits tensor
+    :param input_ph: The TensorFlow tensor for the input
+                    (needed if returning logits)
+                    ("ph" stands for placeholder but it need not actually be a
+                    placeholder)
+    :param img_rows: number of row in the image
+    :param img_cols: number of columns in the image
+    :param channels: number of color channels (e.g., 1 for MNIST)
+    :param nb_filters: number of convolutional filters per layer
+    :param nb_classes: the number of output classes
+    :return:
+    """
+    model = Sequential()
+
+    # Define the layers successively (convolution layers are version dependent)
+    if keras.backend.image_dim_ordering() == 'th':
+        input_shape = (channels, img_rows, img_cols)
+    else:
+        input_shape = (img_rows, img_cols, channels)
+
+    layers = [conv_2d(nb_filters, (5, 5), (1, 1), "same",
+                      input_shape=input_shape),
+              Activation('relu'),
+              conv_2d(nb_filters, (5, 5), (1, 1), "valid"),
+              Activation('relu'),
+	      Flatten(),
+	      Dropout(0.25),
+	      Dense(128),
+	      Activation('relu'),
+	      Dropout(0.5),
+              Dense(nb_classes)]
+
+    for layer in layers:
+        model.add(layer)
+
+    if logits:
+        logits_tensor = model(input_ph)
+    model.add(Activation('softmax'))
+
+    if logits:
+        return model, logits_tensor
+    else:
+        return model
+
+def mnist_fgsm(train_start=0, train_end=60000, test_start=0,
+                   test_end=10000, nb_epochs=6, batch_size=128,
+                   learning_rate=0.001, train_dir=model_path,
+                   filename="mnist.ckpt", load_model=False,
+                   nb_classes=10, testing=False):
+    """
+    MNIST CleverHans tutorial
+    :param train_start: index of first training set example
+    :param train_end: index of last training set example
+    :param test_start: index of first test set example
+    :param test_end: index of last test set example
+    :param nb_epochs: number of epochs to train model
+    :param batch_size: size of training batches
+    :param learning_rate: learning rate for training
+    :param train_dir: Directory storing the saved model
+    :param filename: Filename to save model under
+    :param load_model: True for load, False for not load
+    :param testing: if true, test error is calculated
+    :return: an AccuracyReport object
+    """
+    keras.layers.core.K.set_learning_phase(0)
+
+    # Object used to keep track of (and return) key accuracies
+    report = AccuracyReport()
+
+    # Set TF random seed to improve reproducibility
+    tf.set_random_seed(1234)
+
+    if not hasattr(backend, "tf"):
+        raise RuntimeError("This tutorial requires keras to be configured"
+                           " to use the TensorFlow backend.")
+
+    if keras.backend.image_dim_ordering() != 'tf':
+        keras.backend.set_image_dim_ordering('tf')
+        print("INFO: '~/.keras/keras.json' sets 'image_dim_ordering' to "
+              "'th', temporarily setting to 'tf'")
+
+    # Create TF session and set as Keras backend session
+    sess = tf.Session()
+    keras.backend.set_session(sess)
+
+    # Get MNIST test data
+    X_train, Y_train, X_test, Y_test = data_mnist(train_start=train_start,
+                                                  train_end=train_end,
+                                                  test_start=test_start,
+                                                  test_end=test_end)
+
+    # Use label smoothing
+    assert Y_train.shape[1] == 10
+    label_smooth = .1
+    Y_train = Y_train.clip(label_smooth / 9., 1. - label_smooth)
+
+    # Define input TF placeholder
+    x = tf.placeholder(tf.float32, shape=(None, 28, 28, 1))
+    y = tf.placeholder(tf.float32, shape=(None, 10))
+
+    # Define TF model graph
+    model, logits = cnn_model(logits=True, input_ph=x)
+    preds = model(x)
+    print("Defined TensorFlow model graph.")
+
+    def evaluate():
+        # Evaluate the accuracy of the MNIST model on legitimate test examples
+        eval_params = {'batch_size': batch_size}
+        acc = model_eval(sess, x, y, preds, X_test, Y_test, args=eval_params)
+        report.clean_train_clean_eval = acc
+        assert X_test.shape[0] == test_end - test_start, X_test.shape
+        print('Test accuracy on legitimate examples: %0.4f' % acc)
+
+    # Train an MNIST model
+    train_params = {
+        'nb_epochs': nb_epochs,
+        'batch_size': batch_size,
+        'learning_rate': learning_rate,
+        'train_dir': train_dir,
+        'filename': filename
+    }
+    ckpt = tf.train.get_checkpoint_state(train_dir)
+    ckpt_path = False if ckpt is None else ckpt.model_checkpoint_path
+
+    rng = np.random.RandomState([2017, 8, 30])
+    if load_model and ckpt_path:
+        saver = tf.train.Saver()
+        saver.restore(sess, ckpt_path)
+        print("Model loaded from: {}".format(ckpt_path))
+        evaluate()
+    else:
+        print("Model was not loaded, training from scratch.")
+        model_train(sess, x, y, preds, X_train, Y_train, evaluate=evaluate,
+                    args=train_params, save=True, rng=rng)
+
+    # Calculate training error
+    if testing:
+        eval_params = {'batch_size': batch_size}
+        acc = model_eval(sess, x, y, preds, X_train, Y_train, args=eval_params)
+        report.train_clean_train_clean_eval = acc
+
+    return report
+
+
+def main(argv=None):
+    mnist_fgsm(nb_epochs=FLAGS.nb_epochs,
+                   batch_size=FLAGS.batch_size,
+                   learning_rate=FLAGS.learning_rate,
+                   train_dir=FLAGS.train_dir,
+                   filename=FLAGS.filename,
+		   nb_classes=FLAGS.nb_classes,
+                   load_model=FLAGS.load_model)
+
+
+if __name__ == '__main__':
+    flags.DEFINE_integer('nb_epochs', 6, 'Number of epochs to train model')
+    flags.DEFINE_integer('nb_classes', 10, 'Number of output classes')
+    flags.DEFINE_integer('batch_size', 128, 'Size of training batches')
+    flags.DEFINE_float('learning_rate', 0.001, 'Learning rate for training')
+    flags.DEFINE_string('train_dir', model_path, 'Directory where to save model.')
+    flags.DEFINE_string('filename', 'mnist.ckpt', 'Checkpoint filename.')
+    flags.DEFINE_boolean('load_model', True, 'Load saved model or train.')
+    tf.app.run()
+
